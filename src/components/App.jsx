f770b02907a2797be0e641043fcd3200eb41c2d4
psycrow117 <psycrow117@hotmail.com>
2020-07-25 02:11:03 +0800

    added Login component(WIP)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-100">
       <Header />
       <Content />
       <Footer />
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Login from './Login';
 
 function Content() {
   return (
-    <Container>
-      <Row className="d-flex justify-content-center">
-        <Col>Hello World! 5</Col>
-      </Row>
-    </Container>
+    <div className="h-75 d-flex flex-column justify-content-center align-items-center">
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs="10" sm="6">
+            <Login />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
